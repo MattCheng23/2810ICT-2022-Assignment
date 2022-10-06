b2ef73fc07d3472110e334c4618b325d0b12d576
ChDXXX <870565769@qq.com>
2022-10-06 20:15:20 +1000

    No.4
    Change of No.4

--- No.4.py
+++ No.4.py
@@ -4,30 +4,30 @@ import matplotlib.pyplot as plt
 
 fileName = 'DataBase.csv'
 
-xtime = []
-yvalue = []
+x = []
+y = []
 
 with open(fileName, 'r') as csvfile:
-    spamreader = csv.reader(csvfile)
-    for row in spamreader:
+    reader = csv.reader(csvfile)
+    for row in reader:
 
-        xtimestamp = time.mktime(time.strptime(row[0], "%y%m%d_%H:%M:%S"))
+        xtime = time.mktime(time.strptime(row[0], "%y%m%d_%H:%M:%S"))
 
         if row[2].isdigit():
-            xtime.append(xtimestamp)
-            yvalue.append(row[2])
+            x.append(xtime)
+            y.append(row[2])
         else:
             print("Wrong type: " + str(row))
 
-deta = int(xtime[0])
-for j in range(len(xtime)):
-    xtime[j] = int(xtime[j]) - deta
-    yvalue[j] = int(yvalue[j])
+deta = int(x[0])
+for j in range(len(x)):
+    x[j] = int(x[j]) - deta
+    y[j] = int(y[j])
 
 plt.xlabel("Time(s)")
 plt.ylabel("Ug(TSI_PM2.5)")
 
-plt.plot(xtime, yvalue, label='TSI_PM2.5', linewidth=2)
+plt.plot(x, y, label='TSI_PM2.5', linewidth=2)
 
 plt.legend()
 plt.show()
