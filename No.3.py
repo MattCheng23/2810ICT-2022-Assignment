ff2a20210c0e27248479d2157e58b0fbb2602c5a
ChDXXX <870565769@qq.com>
2022-10-08 00:29:55 +1000

    No3 change

--- No.3.py
+++ No.3.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from datetime import datetime
+import re
 
 
 df = pd.read_csv('./DataBase.csv', low_memory=False)
 
 df['OFFENCE_MONTH'] = df['OFFENCE_MONTH'].apply(pd.to_datetime)
+
 
 # start_time = '2012-3-1'
 # end_time = '2013-6-1'
@@ -14,7 +16,7 @@ end_time = input('Please enter end time(example: 2013-6-1): ')
 print(f'The data from {start_time} to {end_time} are:')
 start_time = datetime.strptime(start_time,'%Y-%m-%d')
 end_time = datetime.strptime(end_time,'%Y-%m-%d')
-mask = (df['OFFENCE_MONTH'] > start_time) & (df['OFFENCE_MONTH'] < end_time)
+mask = (df['OFFENCE_MONTH'] > start_time) & (df['OFFENCE_MONTH'] < end_time) &(df['OFFENCE_DESC'].str.contains('Camera',re.IGNORECASE))
 # print(mask.value_counts())
 print(df[mask])
 df[mask].reset_index(drop=True).to_csv(f'{start_time}_to_{end_time}.csv')
