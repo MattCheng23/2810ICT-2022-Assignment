86e1da767590bce7247074946dab9a3d01963c3f
Matt <1099365940@qq.com>
2022-10-09 14:29:36 +0800

    code

--- wxGUI.py
+++ wxGUI.py
@@ -20,20 +20,20 @@ class MainWindow(wx.Frame):
         self.Bind(wx.EVT_BUTTON, self.OnClick, self.button)
 
         # the edit control - one line version.
-        self.lbldd = wx.StaticText(panel, label="DD:", pos=(20, 110))
-        self.editdd = wx.TextCtrl(panel, value="01", pos=(100, 110), size=(140, -1))
+        self.lbldd = wx.StaticText(panel, label="Start Date:", pos=(20, 110))
+        self.editdd = wx.TextCtrl(panel, value="Enter a date", pos=(100, 110), size=(140, -1))
         self.Bind(wx.EVT_TEXT, self.EvtText, self.editdd)
         self.Bind(wx.EVT_CHAR, self.EvtChar, self.editdd)
 
-        self.lblmm = wx.StaticText(panel, label="MM:", pos=(20, 140))
-        self.editmm = wx.TextCtrl(panel, value="05", pos=(100, 140), size=(140, -1))
+        self.lblmm = wx.StaticText(panel, label="End Date:", pos=(20, 140))
+        self.editmm = wx.TextCtrl(panel, value="Enter a date", pos=(100, 140), size=(140, -1))
         self.Bind(wx.EVT_TEXT, self.EvtText, self.editmm)
         self.Bind(wx.EVT_CHAR, self.EvtChar, self.editmm)
 
-        self.lblyy = wx.StaticText(panel, label="YYYY:", pos=(20, 170))
-        self.edityy = wx.TextCtrl(panel, value="2013", pos=(100, 170), size=(140, -1))
-        self.Bind(wx.EVT_TEXT, self.EvtText, self.edityy)
-        self.Bind(wx.EVT_CHAR, self.EvtChar, self.edityy)
+        #self.lblyy = wx.StaticText(panel, label="YYYY:", pos=(20, 170))
+        #self.edityy = wx.TextCtrl(panel, value="2013", pos=(100, 170), size=(140, -1))
+        #self.Bind(wx.EVT_TEXT, self.EvtText, self.edityy)
+        #self.Bind(wx.EVT_CHAR, self.EvtChar, self.edityy)
 
         self.lblpe = wx.StaticText(panel, label="Type of Traffic penaty:", pos=(20, 200))
         self.editpe = wx.TextCtrl(panel, value="E.g Speeding", pos=(20, 230), size=(200, -1))
